3a3bd796dce049fb155ca21b8c0187b82c75b6b8
chenzhiren <115920670+chenzhiren@users.noreply.github.com>
2022-10-20 08:26:02 +0800

    Add files via upload

---
+++ test/test.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pymysql
+import pandas as pd
+sql='select * from risk limit 10'
+conn=pymysql.connect(host=st.secrets.mysql.host,
+                     port=st.secrets.mysql.port,
+                     user=st.secrets.mysql.user,
+                     passwd=st.secrets.mysql.passwd,
+                     database = st.secrets.mysql.database)
+cur = conn.cursor()
+cur.execute(sql)
+data = cur.fetchall()
+data=pd.DataFrame(data)
+st.dataframe(data)
+print(data)
