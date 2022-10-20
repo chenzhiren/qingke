200c7d8173b9f84f921607c49c73e91e9282f1bd
chenzhiren <115920670+chenzhiren@users.noreply.github.com>
2022-10-20 22:17:43 +0800

    Update invest.py

--- invest/invest.py
+++ invest/invest.py
@@ -14,7 +14,7 @@ if sidebar=='资讯爬取和清洗':
 
    if st.button('清洗数据'):
        # 连接数据库
-       conn = pymysql.connect(host=st.secrets.mysql.host, user=st.secrets.mysql.user, passwd=st.secrets.mysql.password, db=st.secrets.mysql.database, port=st.secrets.mysql.port)
+       conn = pymysql.connect(host=st.secrets.mysql.host, user=st.secrets.mysql.user, passwd=st.secrets.mysql.password, database=st.secrets.mysql.database, port=st.secrets.mysql.port)
        # 游标
        cur = conn.cursor()
        # sql语句，获取最新批次数据
