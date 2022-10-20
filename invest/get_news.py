2506adc3fe694ba14b0d3d809e9cb8c4f3394368
chenzhiren <115920670+chenzhiren@users.noreply.github.com>
2022-10-20 23:06:21 +0800

    Update get_news.py

--- invest/get_news.py
+++ invest/get_news.py
@@ -5,9 +5,16 @@ from lxml import etree
 import pymysql
 import datetime
 import streamlit as st
+from ssh import SSHTunnelForwarder
 
+server=SSHTunnelForwarder(
+       ssh_address_or_host=('192.168.0.107',22),
+       ssh_username=Y7000,
+       ssh_password=999555,
+       remote_bind_address=('localhost',3306)
+    
 # 连接数据库
-conn=pymysql.connect(host=st.secrets.mysql.host, user=st.secrets.mysql.user, passwd=st.secrets.mysql.password, database=st.secrets.mysql.database, port=st.secrets.mysql.port)
+conn=pymysql.connect(host=st.secrets.mysql.host, user=st.secrets.mysql.user, passwd=st.secrets.mysql.password, database=st.secrets.mysql.database, port=server.local_bind_port)
 # 游标
 cur=conn.cursor()
 # 获取批次
